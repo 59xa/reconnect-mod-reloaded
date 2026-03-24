3e722f58a58c4b8c475c36c2f6f38c1d33122499
59xa <59xa@proton.me>
2026-03-24 04:56:22 -0600

    Revert changes

--- neoforge/src/main/java/io/xa59/reconnect/utils/NeoForgeStatusDisplay.java
+++ neoforge/src/main/java/io/xa59/reconnect/utils/NeoForgeStatusDisplay.java
@@ -4,39 +4,32 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 
 @EventBusSubscriber(value = Dist.CLIENT, modid = "reconnect")
 public class NeoForgeStatusDisplay implements IStatusDisplay {
-
-    private static String pendingMessage = null;
-    private static ChatFormatting pendingFormatting = ChatFormatting.WHITE;
+    private boolean showOverlay = false;
 
     @Override
     public void sendOverlayMessageAfterJoin(String message, ChatFormatting formatting) {
-        pendingMessage = message;
-        pendingFormatting = formatting;
+        NeoForge.EVENT_BUS.register(new Object() {
+            @SubscribeEvent
+            public void onPlayerLogin(ClientPlayerNetworkEvent.LoggingIn event) {
+                if (!showOverlay && Minecraft.getInstance().player != null) {
+                    Minecraft.getInstance().player.sendOverlayMessage(
+                            Component.literal(message).withStyle(formatting)
+                    );
+                    showOverlay = true;
+                }
+            }
+        });
     }
 
     @Override
     public void resetOverlay() {
-        pendingMessage = null;
-    }
-
-    @SubscribeEvent
-    public static void onPlayerLogin(ClientPlayerNetworkEvent.LoggingIn event) {
-        if (pendingMessage != null) {
-            Minecraft mc = Minecraft.getInstance();
-
-            if (mc.player != null) {
-                mc.player.sendOverlayMessage(
-                        Component.literal(pendingMessage).withStyle(pendingFormatting)
-                );
-
-                pendingMessage = null;
-            }
-        }
+        showOverlay = false;
     }
 }
