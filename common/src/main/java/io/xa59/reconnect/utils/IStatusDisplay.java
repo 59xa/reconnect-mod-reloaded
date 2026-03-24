12623f47ab4e70414622ad806083b67588a2d387
59xa <59xa@proton.me>
2026-03-24 03:21:37 -0600

    Remove IStatusDisplay.java

--- common/src/main/java/io/xa59/reconnect/utils/IStatusDisplay.java
+++
@@ -1,8 +0,0 @@
-package io.xa59.reconnect.utils;
-
-import net.minecraft.ChatFormatting;
-
-public interface IStatusDisplay {
-    void sendOverlayMessageAfterJoin(String message, ChatFormatting formatting);
-    void resetOverlay();
-}
